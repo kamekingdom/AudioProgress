6d2b01b457422e232b6840ae1f68f09a337bf7f0
kamekingdom <yudainakamura2002@gmail.com>
2025-12-18 23:38:17 +0900

    Initial Commit

---
+++ AudioProgress/AudioProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioProgressApp.swift
+//  AudioProgress
+//
+//  Created by 中村裕大 on 2025/12/18.
+//
+
+import SwiftUI
+
+@main
+struct AudioProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioProgressTests/AudioProgressTests.swift
@@ -0,0 +1,16 @@
+//
+//  AudioProgressTests.swift
+//  AudioProgressTests
+//
+//  Created by 中村裕大 on 2025/12/18.
+//
+
+import Testing
+
+struct AudioProgressTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
